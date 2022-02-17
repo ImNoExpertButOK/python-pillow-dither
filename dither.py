002747be7a172687171f437cb5c20090ac63ff9d
ImNoExpertButOK <66391521+ImNoExpertButOK@users.noreply.github.com>
2022-02-17 15:59:22 -0300

    Create dither.py

---
+++ dither.py
@@ -0,0 +1,24 @@
+from PIL import Image
+
+img = Image.open("suporte/base_image.jpeg").convert("L")
+print("Total pixels:", img.height * img.width)
+
+limit = 128
+
+for v_pixel in range(img.height):
+    for h_pixel in range(img.width):
+
+        old_value = img.getpixel((h_pixel, v_pixel))
+        new_value = 0 if old_value < limit else 255
+        img.putpixel(xy = (h_pixel, v_pixel), value = new_value)
+        error = old_value - new_value
+
+        try:
+            img.putpixel(xy = (h_pixel + 1, v_pixel), value = int(error * 7 / 16 + (img.getpixel((h_pixel + 1, v_pixel)))))
+            img.putpixel(xy = (h_pixel + 1, v_pixel + 1), value = int(error * 1 / 16 + (img.getpixel((h_pixel + 1, v_pixel + 1)))))
+            img.putpixel(xy = (h_pixel, v_pixel + 1), value = int(error * 5 / 16 + (img.getpixel((h_pixel, v_pixel + 1)))))
+            img.putpixel(xy = (h_pixel - 1, v_pixel + 1), value = int(error * 3 / 16 + (img.getpixel((h_pixel - 1, v_pixel + 1)))))
+        except IndexError:
+            pass
+
+img.save("suporte/output.png")
